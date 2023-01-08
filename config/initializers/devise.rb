d52db9af10f381f4107bc16a7381a121f3b8502e
helmut cardenas <cardenas.hcconstrucciones@gmail.com>
2023-01-08 15:44:06 -0300

    modificacion archivo devise para turbo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -39,7 +39,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'jorgitsss@gmail.com'
+  config.mailer_sender = 'helmocktz@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
